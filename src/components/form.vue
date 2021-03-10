be904d49670d7188c54e278e08cbfe5021816a26
Memo Serrano <gmemo_sj@hotmail.com>
2021-03-10 17:58:25 -0600

    form create

---
+++ src/components/form.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="form">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  props: {},
+  methods: {
+    _getData: function() {
+      //ajax
+    }
+  },
+  actions: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
