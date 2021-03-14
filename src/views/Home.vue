3efd394f7d289728a16498d49e297d259c0aa343
serrangh <guillermo.serrano.ext@dematic.com>
2021-03-14 16:03:59 -0400

    create views

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <UserCreate />
+    <UserList />
+  </div>
+</template>
+
+<script>
+import UserCreate from './components/UserCreate.vue'
+import UserList from './components/UserList.vue'
+
+export default {
+  name: 'Home',
+  components: { UserCreate, UserList }
+}
